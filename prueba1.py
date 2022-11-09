5d83b2854181ab5f391bd083263a1bc3af12a597
Angie <angie.ellis@utp.ac.pa>
2022-11-09 10:22:54 -0500

    Primer commit- pruebas en selenium 2

---
+++ prueba1.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome('C:/Selenium/chromedriver')
+driver.maximize_window()
+driver.delete_all_cookies()
+driver.get("https://www.amazon.com/")
+
+buscar = driver.find_element(By.ID, 'twotabsearchtextbox')
+driver.find_element(By.ID, "nav-search-submit-button").click()
+driver.find_element(By.CLASS_NAME, "s-image").click()
+
+driver.find_element(By.ID, 'a-autoid-0-announce').click()
+driver.find_element(By.ID, 'quantity_3').click()
+driver.find_element(By.ID, "add-to-cart-button").click()
+driver.find_element(By.ID, "sw-gtc").click()
+
+time.sleep(100)
+driver.close()
